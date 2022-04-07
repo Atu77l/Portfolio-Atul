d6cabc1a564ec2407f81cfbd49e382f1c976bf10
Atul Kesharwani <60789960+Atu77l@users.noreply.github.com>
2022-04-07 13:01:15 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/Atu77l/Portfolio-Atul'
       className='link footer__link'
     >
       Created By Atul kesharwani
